696c3f373e69865e957b4a86a0515540f100c302
Fauwaaz <fauwaazshaikh999@gmail.com>
2025-09-19 17:20:41 +0530

    New pages fixed

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -5,7 +5,6 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useSidebar } from "../context/SidebarContext";
 import {
-  CalenderIcon,
   ChevronDownIcon,
   GridIcon,
   HorizontaLDots,
